b36128e030b74fbb5ce6d9d63900e360fc0b2500
Josh Mabry <mabry1985@gmail.com>
2021-02-13 20:20:06 -0800

    render and style button svgs

--- src/components/jm-weather-widget-container/jm-weather-widget-container.tsx
+++ src/components/jm-weather-widget-container/jm-weather-widget-container.tsx
@@ -1,5 +1,7 @@
 import { Component, Host, h } from '@stencil/core';
 import Sun from './assets/sun.svg';
+import Menu from './assets/menu.svg';
+import DownArrow from './assets/arrow-down.svg';
 
 @Component({
   tag: 'jm-weather-widget-container',
@@ -11,10 +13,14 @@ export class JmWeatherWidgetContainer {
   render() {
     return (
       <Host>
-        <div class="svg-container" innerHTML={Sun}></div>
+        <div class="weather-icon-container" innerHTML={Sun} />
         <div class="info-container">
           <p class="temp">29f</p>
           <h4 class="location">Portland, OR</h4>
+        </div>
+        <div class="button-container">
+          <div innerHTML={Menu} class="hamburger-menu"/>
+          <div innerHTML={DownArrow} class="dropdown-button"/>
         </div>
       </Host>
     );
